a3639a7022991baccb6ad338c0d553e626ac1ba5
tremainebuchanan <tremainekbuchanan@gmail.com>
2015-11-16 14:30:32 -0500

    Changed verbs from all to get

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,3 @@
-/**
- * This file is where you define your application routes and controllers.
- * 
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- * 
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- * 
- * Each of these files is a route controller, and is responsible for all the
- * processing that needs to happen for the route (e.g. loading data, handling
- * form submissions, rendering the view template, etc).
- * 
- * Bind each route pattern your application should respond to in the function
- * that is exported from this module, following the examples below.
- * 
- * See the Express application routing documentation for more information:
- * http://expressjs.com/api.html#app.VERB
- */
-
 var keystone = require('keystone');
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
@@ -29,7 +9,6 @@ keystone.pre('render', middleware.flashMessages);
 // Import Route Controllers
 var routes = {
 	views: importRoutes('./views')
-	//api: importRoutes('./api')
 };
 
 // Setup Route Bindings
@@ -40,8 +19,8 @@ exports = module.exports = function(app) {
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
-	app.all('/contact', routes.views.contact);
-	app.all('/about', routes.views.about);
-	app.all('/devca', routes.views.devca);
+	app.get('/contact', routes.views.contact);
+	app.get('/about', routes.views.about);
+	app.get('/devca', routes.views.devca);
 
 };
