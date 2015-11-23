dd7870b30a462cca45e0a918d65797a51ced835c
tremainebuchanan <tremainekbuchanan@gmail.com>
2015-11-23 10:40:54 -0500

    Optimizing api

--- routes/api/post.js
+++ routes/api/post.js
@@ -3,25 +3,38 @@
 		Post = keystone.list('Post'),
 		User = keystone.list('User');
 	/**
-	 * [index description]
+	 * Lists all posts. 
 	 * @param  {[type]} req [description]
 	 * @param  {[type]} res [description]
 	 * @return {[type]}     [description]
 	 */
 	exports.index = function(req, res){
-		User.model.find()
-			.where('name.first','Matthew')
-			.where('name.last', 'McNaughton')
-			.exec(function(err, user){
-				Post.model
-					.find()
-					.where('author', user[0]._id)
-					.populate('author', 'name')
-					.limit(req.query.limit)
-					.skip(req.query.skip)
-					.exec(function(err, posts) {
-							res.send(posts);
-					}); 
-			});
+		
 	};
+
+	function getPostsByAuthor(author){
+		
+	}
+
+	/**
+	 * List posts by author
+	 */
+	exports.author = function(req, res){
+		console.log(req.query);
+		res.send({message: 'Author'});
+		// User.model.find()
+		// 	.where('name.first',author.first_name)
+		// 	.where('name.last', author.last_name)
+		// 	.exec(function(err, user){
+		// 		Post.model
+		// 			.find()
+		// 			.where('author', user[0]._id)
+		// 			.populate('author', 'name')
+		// 			.limit(req.query.limit)
+		// 			.skip(req.query.skip)
+		// 			.exec(function(err, posts) {
+		// 					res.send(posts);
+		// 			}); 
+		// 	});
+	}
 })();
