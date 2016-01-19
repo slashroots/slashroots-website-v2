3fa6988a788cadb5f3f68e71d281cf6db6f94676
tremainebuchanan <tremainekbuchanan@gmail.com>
2016-01-19 15:47:16 -0500

    Fixed issue

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,6 @@ exports = module.exports = function(app) {
 	app.get('/devca', routes.views.devca);
 	app.get('/author/:name', routes.views.posts);
 	//API Routes
-	app.get('/api/post/list', keystone.initAPI, routes.api.posts.list);
-	//app.get('/posts', routes.api.post.index);
+	//app.get('/api/post/list', keystone.initAPI, routes.api.posts.list);
+	app.get('/posts', routes.api.post.index);
 };
